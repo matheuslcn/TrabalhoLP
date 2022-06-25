2978c3bbafadd19d77f9cd5100596a6b9180edd7
matheuslcn <71265929+matheuslcn@users.noreply.github.com>
2022-06-25 18:31:16 -0300

    refazendo o inicio

--- haskell/lp.hs
+++ haskell/lp.hs
@@ -1,11 +1,43 @@
-anda' :: (Int, Int, Char) -> String -> (Bool, String)
-anda' a b = (True, "ixtringui")
+loop :: [(Int, Int, Char)] -> String -> Int -> Bool
+loop x y z
+    | head y == '?' = teste x (drop 2 y) 1
+    | head y == 'U' = escolha x (drop 2 y) 1
+    | head y == '*' = loop x (drop 2 y) 1
+    | head y == ';' = sequencial x (drop 2 y) 1
+    | otherwise = False
+
+teste :: [(Int, Int, Char)] -> String -> Int -> Bool
+teste x y z
+    | head y == '?' = teste x (drop 2 y) 1
+    | head y == 'U' = escolha x (drop 2 y) 1
+    | head y == '*' = loop x (drop 2 y) 1
+    | head y == ';' = sequencial x (drop 2 y) 1
+    | otherwise = False
+
+escolha :: [(Int, Int, Char)] -> String -> Int -> Bool
+escolha x y z
+    | head y == '?' = teste x (drop 2 y) 1
+    | head y == 'U' = escolha x (drop 2 y) 1
+    | head y == '*' = loop x (drop 2 y) 1
+    | head y == ';' = sequencial x (drop 2 y) 1
+    | otherwise = False
+
+sequencial :: [(Int, Int, Char)] -> String -> Int -> Bool
+sequencial x y z
+    | head y == '?' = teste x (drop 2 y) 1
+    | head y == 'U' = escolha x (drop 2 y) 1
+    | head y == '*' = loop x (drop 2 y) 1
+    | head y == ';' = sequencial x (drop 2 y) 1
+    | otherwise = False
+
+
+anda' :: [(Int, Int, Char)] -> String -> Int -> (Bool, String)
+anda' a b c = 
 
 
 anda :: [(Int, Int, Char)] -> String -> Bool
 anda [] a = not (null a)
 anda a [] = not (null a)
-anda x y = do
-    let (resp, prox) = anda' (head x) y
-    if resp then anda (tail x) prox
-    else resp
+anda x y = resp
+    where (resp, _) = anda' x y 0
+
