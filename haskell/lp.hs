f6d334b6ab721525c521e292eda070e219bcae6b
matheuslcn <71265929+matheuslcn@users.noreply.github.com>
2022-06-27 16:23:25 -0300

    teste e inicio do loop

--- haskell/lp.hs
+++ haskell/lp.hs
@@ -5,51 +5,46 @@ pegaSegundo (_,x,_) = x
 pegaTerceiro :: (a, b, c) -> c
 pegaTerceiro (_,_,x) = x
 
-loop :: [(Int, Int, Char)] -> String -> (Bool, String)
+loop :: [(Int, Int, Char)] -> String -> (Bool, [(Int, Int, Char)], String)
 loop x y
-    | (pegaTerceiro (head x) == head y) && (head (tail y) == ')') = loop (tail x) y
---    | x = anda' x y caso o loop n tenha um elemento só
-    | otherwise = anda' x y
+    | not (null x) && (pegaTerceiro (head x) == head y) && (head (tail y) == ')') = loop (tail x) y
+    | otherwise = do -- TA COM PROBLEMA 
+        let (r, a, b) = anda' x y
+        if r then loop a y else anda' x b
 
-teste :: [(Int, Int, Char)] -> String -> (Bool, String)
+teste :: [(Int, Int, Char)] -> String -> (Bool, [(Int, Int, Char)], String)
 teste x y
-    | head y == '?' = teste x (drop 2 y)
-    | head y == 'U' = escolha x (drop 2 y)
-    | head y == '*' = loop x (drop 2 y)
-    | head y == ';' = sequencial x (drop 2 y)
+    | not (null x) && head (tail y) == ')' = (True, x, drop 2 y)
     | otherwise = anda' x y
 
-escolha :: [(Int, Int, Char)] -> String -> (Bool, String)
+escolha :: [(Int, Int, Char)] -> String -> (Bool, [(Int, Int, Char)], String)
 escolha x y = do
-    let (r, prox) = anda' x y
-    if r then (r, prox) else anda' x prox
+    let (r, a, prox) = anda' x y
+    if r then (r, a, prox) else anda' x prox
 
-sequencial :: [(Int, Int, Char)] -> String -> (Bool, String)
+sequencial :: [(Int, Int, Char)] -> String -> (Bool, [(Int, Int, Char)], String)
 sequencial x y
-    | (pegaTerceiro (head x) == head y) && (head (tail y) == ')') = (True, drop 2 y)
-    | (pegaTerceiro (head x) == head y) && (head (tail y) == ',') = sequencial (tail x) (drop 2 y)
+    | not (null x) && (pegaTerceiro (head x) == head y) && (head (tail y) == ')') = (True, tail x, drop 2 y)
+    | not (null x) && (pegaTerceiro (head x) == head y) && (head (tail y) == ',') = sequencial (tail x) (drop 2 y)
     | otherwise = anda' x y
 
-negacao :: [(Int, Int, Char)] -> String -> (Bool, String)
+negacao :: [(Int, Int, Char)] -> String -> (Bool, [(Int, Int, Char)], String)
 negacao x y = do
-    let (r, prox) = anda' x y
-    (not r, prox)
+    let (r, a, prox) = anda' x y
+    (not r, a, prox)
 
-anda' :: [(Int, Int, Char)] -> String -> (Bool, String)
-anda' [] x = (null x, x)
-anda' x [] = (null x, "")
+anda' :: [(Int, Int, Char)] -> String -> (Bool, [(Int, Int, Char)], String)
+anda' [] a = (null a, [],a)
+anda' a [] = (null a, a,"")
 anda' x y
     | head y == '?' = teste x (drop 2 y)
     | head y == 'U' = escolha x (drop 2 y)
     | head y == '*' = loop x (drop 2 y)
     | head y == ';' = sequencial x (drop 2 y)
     | head y == '!' = negacao x (drop 2 y)
-    | otherwise = (False, y)
+    | otherwise = (False, x, y)
 
 anda :: [(Int, Int, Char)] -> String -> Bool
-anda [] a = null a
-anda a [] = null a
 anda x y = do
-    let (resp, prox) = anda' x y
+    let (resp, a, b) = anda' x y
     resp
-
